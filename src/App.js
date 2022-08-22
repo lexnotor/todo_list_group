51055954d19af0de38f671bed8c3a4e1b232be57
samuelmbabhazi <samuelmbabhazi42@gmail.com>
2022-08-22 12:26:46 +0200

    create function addTask and removeTask

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  const [input, setInput] = useState();
+  const [tasks, setTasks] = useState([]);
+
+  //Fonction Add un task
+  const addTask = (event) => {
+    event.preventDefault();
+    setTasks([...tasks, { id: Math.random(), title: input }]);
+    setInput("");
+  };
+
+  //Fonction  delete un task
+  const removeTask = ({ id }) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+ 
+
+  return <div className="App"></div>;
 }
 
 export default App;
