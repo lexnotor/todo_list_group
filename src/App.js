f54d08cd373a3593931bb3e2d21223a87b19f515
RollyLif <relifungula@gmail.com>
2022-08-22 14:32:11 +0200

    save

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <ul>
+
+      </ul>
+      <Footer />
+
       
     </div>
   );
