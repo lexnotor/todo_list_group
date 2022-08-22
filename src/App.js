6eab29a5f1c95ddf4cf8805873428bed0df48cf7
samuelmbabhazi <samuelmbabhazi42@gmail.com>
2022-08-22 15:18:32 +0200

    new fonctionality

--- src/App.js
+++ src/App.js
@@ -2,21 +2,27 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [input, setInput] = useState();
   const [tasks, setTasks] = useState([]);
 
   //Fonction Add un task
-  const addTask = (event) => {
-    event.preventDefault();
-    setTasks([...tasks, { id: Math.random(), title: input }]);
-    setInput("");
+  const addTask = (tache) => {
+    if (tache.trim() !== "") {
+      setTasks([...tasks, { id: Math.random(), title: tache, check: false }]);
+    }
   };
 
   //Fonction  delete un task
+
   const removeTask = ({ id }) => {
     setTasks(tasks.filter((task) => task.id !== id));
   };
- 
+
+  //Fonction delete all
+  const removeAllTask = (check) => {
+    setTasks(tasks.filter((task) => task.check !== false));
+  };
+
+  //Fonction checked
 
   return <div className="App"></div>;
 }
