b65ca4a6dda8416c2a5f77dfc8dbd4cdfd5e41e4
alexissengangabo07 <alexxngabo@gmail.com>
2022-08-23 13:44:22 +0200

    style: header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,29 @@
-import React from 'react'
-import { FaTrashAlt, FaRegCheckSquare, FaTasks } from 'react-icons/fa'
+import React from 'react';
+import '../App.css';
+import { FaTrashAlt, FaRegCheckSquare, FaTasks } from 'react-icons/fa';
 
 const Header = ({ checkAll, deleteAll }) => {
 
   return (
     <div className='header'>
-      <h3>
-        <FaTasks /> 
+      <div>
+        <h3>
+          <FaTasks />
+        </h3>
+      </div>
+      <div>
         La todo list
-      </h3>
-
+      </div>
       <div className='actions'>
-
-        <span onClick={() => checkAll()}> 
-          <FaRegCheckSquare /> 
+        <span onClick={() => checkAll()}>
+          <FaRegCheckSquare />
         </span>
-
-        <span onClick={() => deleteAll()}> 
-          <FaTrashAlt /> 
+        <span onClick={() => deleteAll()}>
+          <FaTrashAlt />
         </span>
 
       </div>
-      
+
     </div>
   )
 }
